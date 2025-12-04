7c4abb4dd6343b5524e13a5e2a7ef9c4845ef7ec
sixnathan <dev@sixnathan.com>
2025-12-04 22:09:53 +0000

    update

--- src/app/prk/v1linreg/page.tsx
+++ src/app/prk/v1linreg/page.tsx
@@ -11,27 +11,23 @@ export default function V1LinearRegression() {
 
 class LinearRegression:
     def __init__(self):
-        self.theta = np.zeros((3,1))
+        self.theta = None
 
-    def hypo(self, test_x):
-        h = test_x @ self.theta
-        return h
+    def hypo(self, X):
+        return X @ self.theta
 
-    def cost(self,X, y, h):
+    def cost(self, X, y):
+        h = self.hypo(X)
         loss = h - y
-        squareloss = loss.T @ loss
-        c = squareloss[0,0]/(2*X.shape[0])
-        return c, loss
-
+        cost = (loss.T @ loss)[0,0] / (2 * X.shape[0])
+        return cost
 
     def graddec(self, X, y, rate, iterations):
+        self.theta = np.zeros((X.shape[1], 1))
         for i in range(iterations):
-            h = X @ self.theta
-            newgrad = (1/(X.shape[0])) * X.T @ (h - y)
-            self.theta = self.theta - rate * newgrad
-            loss = h - y
-            squareloss = loss.T @ loss
-            print(squareloss[0,0])`}</code>
+            h = self.hypo(X)
+            newgrad = (1/X.shape[0]) * X.T @ (h - y)
+            self.theta = self.theta - rate * newgrad`}</code>
         </pre>
       </section>
     </div>
